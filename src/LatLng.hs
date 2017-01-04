8c2b596731ff3bb53970f8208fe32599554fac17
Daniele Francesconi <dfrancesconi12@gmail.com>
2017-01-04 22:18:09 +0000

    new latlng builder

--- src/LatLng.hs
+++ src/LatLng.hs
@@ -24,8 +24,8 @@ data DMSPoint = DMSPoint { degrees :: Double
                          , seconds :: Double
                          }
 
-mkLatLng :: LatitudeDMS -> LongitudeDMS -> Datum -> Except String LatLng
-mkLatLng lat lng dtm = do
+dmsToLatLng :: LatitudeDMS -> LongitudeDMS -> Datum -> Except String LatLng
+dmsToLatLng lat lng dtm = do
   lt <- withExcept (const "Invalid latitude") (evalLatitude lat)
   ln <- withExcept (const "Invalid longitude") (evalLongitude lng)
   let p = LatLngPoint { latitude = lt , longitude = ln, height = 0 }
@@ -43,3 +43,20 @@ mkLatLng lat lng dtm = do
         dmsToLatLngPoint DMSPoint { degrees = d, minutes = m, seconds = s } cardinal
           | d < 0.0 || m < 0.0 || s < 0.0 || d > 90.0 || m >= 60.0 || s >= 60.0 = throwError "Invalid point"
           | otherwise = pure (cardinal * (d + m / 60.0 + s / 3600.0))
+
+
+latLng :: Double -> Double -> Double -> Datum -> Except String LatLng
+latLng lat lng h dtm = do
+  lt <- withExcept (const $ "Latitude (" ++ show lat ++ ") is invalid. Must be between -90.0 and 90.0 inclusive.") (validateLatitude lat)
+  ln <- withExcept (const $ "Longitude (" ++ show lng ++ ") is invalid. Must be between -180.0 and 180.0 inclusive.") (validateLongitude lng)
+  let p = LatLngPoint { latitude = lt , longitude = ln, height = h }
+  pure LatLng { point = p , datum = dtm }
+  where
+        validateLatitude :: Double -> Except String Double
+        validateLatitude l
+          | l < -90.0 || l > 90.0 = throwError "Invalid latitude"
+          | otherwise = pure l
+        validateLongitude :: Double -> Except String Double
+        validateLongitude l
+          | l < -180.0 || l > 180.0 = throwError "Invalid longitude"
+          | otherwise = pure l
